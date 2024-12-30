55bad0433a5ca4c9cb66394be30c5b65e8f94c35
Cstowe01 <cstowe@eagles.nccu.edu>
2024-12-29 23:59:24 -0500

    draft: heat map

--- src/pages/ui/maps/GoogleMaps/index.tsx
+++ src/pages/ui/maps/GoogleMaps/index.tsx
@@ -2,7 +2,7 @@ import { GoogleApiWrapper } from 'google-maps-react'
 
 // components
 import { PageBreadcrumb } from '../../../../components'
-import { BasicGoogleMap, DarkStyledMap, LightStyledMap, MapWithMarkers, PolyLineMap, StreetViewMap } from './data'
+import { BasicGoogleMap, DarkStyledMap, LightStyledMap, MapWithMarkers, PolyLineMap, StreetViewMap, BasicHeatMap } from './data'
 
 interface MapContainerProps {
 	google: any
@@ -14,6 +14,7 @@ const GoogleMaps = ({ google }: MapContainerProps) => {
 			<PageBreadcrumb title="Google Maps" subName="Maps" />
 			<div className="grid lg:grid-cols-2 grid-cols-1 gap-6">
 				<BasicGoogleMap google={google} />
+				<BasicHeatMap google={google}/>
 				<MapWithMarkers google={google} />
 				<StreetViewMap google={google} />
 				<PolyLineMap google={google} />
