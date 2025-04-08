344a0db6875829a06f1e893b1313336ac2f6d2bf
DBH Coder <reanimixtv@gmail.com>
2025-04-08 09:01:56 -0400

    age compare update

--- src/pages/dashboard/dmc/components/AgeComepare.tsx
+++ src/pages/dashboard/dmc/components/AgeComepare.tsx
@@ -28,13 +28,13 @@ const AgeChart = () => {
         series: [
             {
                 name: 'Black',
-                data: [2, 9, 87, 207, 22],
+                data: [2, 9, 87, 50, 157, 22],
             },
         ],
         xaxis: {
-            categories: ['Young Child (4-7)', 'Child (8-10)', 'Pre-Teen (11-13)', 'Teen (14-16)', 'Young Adult (16-19)'],
+            categories: ['Young Child (4-7)', 'Child (8-10)', 'Pre-Teen (11-13)', 'Teen (14-15)', 'Juvenile (16-17)', 'Young Adult (18-25)'],
         },
-        colors: ['#39afd1', '#6c757d', '#ffbc00', '#e3eaef', '#3e60d5'],
+        colors: ['#39afd1', '#6c757d', '#ffbc00', '#e3eaef', '#e3eaef', '#3e60d5'],
         tooltip: {
             y: {
                 formatter: function (value) {
@@ -80,7 +80,7 @@ const AgeChart = () => {
                             <h2>Police Contact by Age Group - Analysis</h2>
                             <h3>Key Insights:</h3>
                             <ul>
-                                <li><strong>Teenagers (14-16) Have the Highest Contact:</strong> With <strong>207 interactions</strong>, this group experiences the most engagement with police, significantly surpassing all other age groups.</li>
+                                <li><strong>Teenagers/Juveniles (14-17) Have the Highest Contact:</strong> With a total<strong>207 interactions</strong> with <strong>157</strong> belonging to Juveniles, this group experiences the most engagement with police, significantly surpassing all other age groups.</li>
                                 <li><strong>Pre-Teens (11-13) Show Significant Contact:</strong> At <strong>87 interactions</strong>, this group also has substantial police presence, though much lower than teens.</li>
                                 <li><strong>Minimal Contact for Younger Children (4-10):</strong> The <strong>4-7</strong> and <strong>8-10</strong> age groups have very low police interactions (<strong>barely registering and 9, respectively</strong>), indicating limited engagement.</li>
                                 <li><strong>Sharp Decline After Age 16:</strong> Contact drops drastically from <strong>207 (14-16 years old) to just 22 (16-19 years old)</strong>, suggesting a major shift in police interactions as teens transition to young adulthood.</li>
